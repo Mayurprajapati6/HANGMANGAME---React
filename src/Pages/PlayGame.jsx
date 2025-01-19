cd935174045d2738610398698e157915ec65a754
Mayur Prajapati <mproyur685@gmail.com>
2025-01-20 01:57:32 +0530

    Added HangMan

--- src/Pages/PlayGame.jsx
+++ src/Pages/PlayGame.jsx
@@ -1,6 +1,9 @@
 import { Link, useLocation, useParams } from "react-router-dom";
 import TextInputFormContainer from "../components/TextInputForm/TextInputFormContainer";
 import MaskedText from "../components/MaskedText/MaskedText";
+import LetterButtons from "../components/LetterButtons/LetterButtons";
+import { useState } from "react";
+import HangMan from "../components/HANGMAN/HangMan";
 
 function PlayGame() {
 
@@ -13,13 +16,39 @@ function PlayGame() {
 
     const { state } = useLocation();
 
+    const [guessedLetters , setGuessedLetters] = useState([]);
+
+    const [step,setStep] = useState(0);
+
+    function handleLetterClick(letter) {
+        if(state.wordSelected.toUpperCase().includes(letter)) {
+            console.log("Correct");
+        }else {
+            console.log("Wrong");
+            setStep(step+1);
+
+        }
+
+        setGuessedLetters([...guessedLetters , letter]);
+    }
+
 
     return(
         <>
             {/*<h1>Play Game{text} {id}</h1> */}
-            <h1>Play Game {state.wordSelected}</h1>
+            <h1>Play Game </h1> {/*{state.wordSelected}*/ }
 
-            <MaskedText text={state.wordSelected} guessedLetters={[]} />
+            <MaskedText text={state.wordSelected} guessedLetters={guessedLetters} />
+
+            <div>
+            <LetterButtons text={state.wordSelected} guessedLetters={guessedLetters} onLetterClick={handleLetterClick}/>
+            </div>
+
+            <div>
+                <HangMan step={step} />
+            </div>
+
+            
 
             <Link to='/start' className="text-green-400">Start Game Link</Link>
         </>
